2fefebb97b1374d9d7af1900adcac69626e1b442
a5medHub <ayassine.auce@gmail.com>
2022-10-27 11:14:09 +0300

    is else

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
-
 console.log(sum);
+if (sum < 21) {
+  console.log("would you like to take oone more card ?");
+} else if (sum === 21) {
+  console.log("you are a winner");
+} else {
+  console.log("sorry you have lost");
+}
