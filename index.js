107a2e92074a1fc7556da19eb1e5ff7aec7122b4
a5medHub <ayassine.auce@gmail.com>
2022-10-27 10:49:24 +0300

    max and min random

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+/*
 let firstCard = Math.random(2, 8);
 let secondCard = Math.random(2, 8);
 let sum = firstCard + secondCard;
 console.log(firstCard);
 console.log(secondCard);
 console.log(sum);
+*/
+let min = 0;
+let max = 0;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+console.log(getRandomInt(2, 11));
