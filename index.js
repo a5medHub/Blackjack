9318c2090fc8fbe579de86d7e982ed6b9841cb81
a5medHub <ayassine.auce@gmail.com>
2022-10-30 08:41:20 +0200

    add var messageEl in js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
+let messageEl = document.getElementById("message-el");
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
