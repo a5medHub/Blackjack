8f24ddb4bb11c7f8867d5bf5b5ddd5af5de93b8c
a5medHub <ayassine.auce@gmail.com>
2022-11-03 06:03:22 +0200

    add if statment to function Game

--- index.js
+++ index.js
@@ -36,4 +36,15 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 console.log(messageEl);
 
-function startGame() {}
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
