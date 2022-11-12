999d4783924917dc7d3d5c60be69981e20e463a1
a5medHub <ayassine.auce@gmail.com>
2022-11-12 19:07:14 +0200

    add if statment to render card

--- index.js
+++ index.js
@@ -48,10 +48,6 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  // Push the card to the cards array
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === true) {
+  }
 }
